17d590060468ca34292dfb0c293f2e47acb00249
benjaminhonorio <benjaminhonorio.12@gmail.com>
2022-01-23 20:30:42 -0500

    Add mercadopago checkout

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,13 +1,15 @@
+import "dotenv/config"
 import express from "express"
 import mongoose from "mongoose"
 import Product from "./models/product.js"
 import Order from "./models/order.js"
+import mercadopago from "mercadopago"
 
 const router = express.Router()
 
 router.get("/products", async (req, res, next) => {
   try {
-    const products = await Product.find()
+    const products = await Product.find({})
     res.json(products)
   } catch (e) {
     next(e)
@@ -24,14 +26,24 @@ router.post("/products", async (req, res, next) => {
   }
 })
 
-router.post("/orders", async (req, res) => {
-  const { products } = req.body
-  for (let i = 0; i < products.length; i++) {
-    const product = await Product.findById(new mongoose.Types.ObjectId(products[i])).lean()
-    products[i] = product
+mercadopago.configure({
+  access_token: process.env.MERCADOPAGO_ACCESS_TOKEN,
+})
+
+router.post("/orders", async (req, res, next) => {
+  const products = req.body
+  try {
+    for (let i = 0; i < products.length; i++) {
+      const product = await Product.findById(new mongoose.Types.ObjectId(products[i])).lean()
+      products[i] = product
+    }
+    const items = products.map((product) => ({ title: product.name, unit_price: product.price, quantity: 1 }))
+    const { response } = await mercadopago.preferences.create({ items })
+    const order = await Order.create({ products })
+    res.json({ order, preferenceId: response.id })
+  } catch (error) {
+    next(error)
   }
-  const order = await Order.create({ products })
-  res.json(order)
 })
 
 export default router
